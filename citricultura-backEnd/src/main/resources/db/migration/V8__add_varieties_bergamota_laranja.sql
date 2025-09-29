ec62df8843df09386063fad505e337a5fc7ea061
ulisses953 <ulisseskranzdamotta@gmail.com>
2025-09-28 22:05:54 -0300

    Inicio do desenvolvimento do frontEnd

---
+++ citricultura-backEnd/src/main/resources/db/migration/V8__add_varieties_bergamota_laranja.sql
@@ -0,0 +1,26 @@
+INSERT INTO fruit (name) VALUES ('Bergamota Ponkan');
+INSERT INTO fruit (name) VALUES ('Bergamota Murcott');
+INSERT INTO fruit (name) VALUES ('Bergamota Montenegrina');
+
+INSERT INTO fruit (name) VALUES ('Laranja Pera');
+INSERT INTO fruit (name) VALUES ('Laranja Bahia (Navel)');
+INSERT INTO fruit (name) VALUES ('Laranja Valência');
+INSERT INTO fruit (name) VALUES ('Laranja Hamlin');
+INSERT INTO fruit (name) VALUES ('Laranja Seleta');
+INSERT INTO fruit (name) VALUES ('Laranja Rubi');
+INSERT INTO fruit (name) VALUES ('Laranja Natal');
+
+INSERT INTO fruit (name) VALUES ('Limão Tahiti');
+INSERT INTO fruit (name) VALUES ('Limão Siciliano');
+INSERT INTO fruit (name) VALUES ('Limão Galego');
+INSERT INTO fruit (name) VALUES ('Limão Cravo');
+INSERT INTO fruit (name) VALUES ('Lima-da-pérsia');
+INSERT INTO fruit (name) VALUES ('Lima Verde');
+
+INSERT INTO fruit (name) VALUES ('Toranja');
+INSERT INTO fruit (name) VALUES ('Pomelo');
+INSERT INTO fruit (name) VALUES ('Cidra');
+INSERT INTO fruit (name) VALUES ('Kumquat');
+INSERT INTO fruit (name) VALUES ('Tangerina Dancy');
+INSERT INTO fruit (name) VALUES ('Tangerina Cleópatra');
+
